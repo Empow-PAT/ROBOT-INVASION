c1d8f727551584d7a66f12d661d56635090b3c5b
geval <gevalera08@gmail.com>
2021-10-06 18:24:16 -0500

    DRONE ARMY ATTACKS FINALY

--- drones.py
+++ drones.py
@@ -1,52 +1,127 @@
 import pygame
 import random
 
+
 windowwidth = 800
 windowheight = 800
 black = (0, 0, 0)
 white = (255,255,255)
+grey = (134,134,134)
+mint_green = (111,222,123)
+red = (255,0,0)
+purple_blue = (138,43,226)
+
+class Dummy:
+    enemyloc = []
+    def __init__(self):
+        self.x = 100
+        self.y = 100
+        self.health = 20
+        self.width = 20
+        self.height = 20
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        Dummy.enemyloc.append(self)
+    def tick(self, keys, win):
+        if self.health <= 0:
+            Dummy.enemyloc.remove(self)
+            return
+
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, mint_green, self.rect)
+
+class Fire:
+    fireloc = []
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.health = 1
+        self.width = 1
+        self.height = 1
+        self.xvel = 0
+        self.yvel = 0
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        Fire.fireloc.append(self)
+    def tick(self, win, firex, firey):
+        for e in Dummy.enemyloc:
+            if e.rect.colliderect(self.rect):
+                e.health -= self.health
+                self.health -= 1
+                Fire.fireloc.remove(self)
+                return
+        if self.health <= 0:
+            Fire.fireloc.remove(self)
+            return
+
+        if firex < self.x:
+            self.xvel = -1
+        if firex > self.x:
+            self.xvel = 1
+        if firey < self.y:
+            self.yvel = -1
+        if firey > self.y:
+            self.yvel = 1
+
+        self.x += self.xvel
+        self.y += self.yvel
+
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, purple_blue, self.rect)
 
 class Drone:
     droneloc = []
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.width = 20
-        self.height = 20
+        self.x = 400
+        self.y = 400
+        self.width = 17
+        self.height = 17
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.xgoal = 0
-        self.ygoal = 0
+        self.xgoal = 400
+        self.ygoal = 400
+        self.color = white
         Drone.droneloc.append(self)
     def tick(self, keys, win):
+        if self.y == self.ygoal and self.x == self.xgoal:
+            self.color = white
+        if self.y != self.ygoal and self.x != self.xgoal:
+            self.color = grey
         for d in Drone.droneloc:
             if d != self and d.rect.colliderect(self.rect):
                 xory = random.randint(0,3)
                 if xory == 1:
-                    self.x = d.xgoal + d.width + 5
-                    self.xgoal = d.xgoal + d.width + 5
+                    self.x = d.x + d.width + 2
+                    self.xgoal = d.xgoal + d.width + 2
                 if xory == 0:
-                    self.y = d.ygoal + d.height + 5
-                    self.ygoal = d.ygoal + d.height + 5
+                    self.y = d.y + d.height + 2
+                    self.ygoal = d.ygoal + d.height + 2
                 if xory == 2:
-                    self.x = d.xgoal - d.width - 5
-                    self.xgoal = d.xgoal - d.width - 5
+                    self.x = d.x - d.width - 2
+                    self.xgoal = d.xgoal - d.width - 2
                 if xory == 3:
-                    self.y = d.ygoal - d.height - 5
-                    self.ygoal = d.ygoal - d.height - 5
+                    self.y = d.y - d.height - 2
+                    self.ygoal = d.ygoal - d.height - 2
         if keys[pygame.K_e] == True:
             Mx,My = pygame.mouse.get_pos()
             Mx += -10
             My += -10
             self.xgoal,self.ygoal = Mx,My
-
         if self.x < self.xgoal:
-            self.x += 5
+            self.x += 1
         if self.x > self.xgoal:
-            self.x += -5
+            self.x += -1
         if self.y < self.ygoal:
-            self.y += 5
+            self.y += 1
         if self.y > self.ygoal:
-            self.y += -5
+            self.y += -1
+        for e in Dummy.enemyloc:
+            if (self.x - e.x <= 200 and self.x - e.x >= -200) and (self.y - e.y <= 200 and self.y - e.y >= -200):
+                self.color = red
+                self.firex = e.x
+                self.firey = e.y
+                if len(Fire.fireloc) <= 10:
+                    fire = Fire(self.x,self.y)
+                    Fire.fireloc.append(fire)
+                for f in Fire.fireloc:
+                    f.tick(win, self.firex, self.firey)
 
         if self.x > windowwidth - self.width:
             self.x = windowwidth - self.width
@@ -58,4 +133,4 @@ class Drone:
             self.y = 0
 
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        pygame.draw.rect(win, white, self.rect)
+        pygame.draw.rect(win, self.color, self.rect)
--- main.py
+++ main.py
@@ -10,95 +10,34 @@ pygame.init()
 
 windowwidth = 800
 windowheight = 800
+
 win = pygame.display.set_mode((windowwidth, windowheight))
 pygame.display.set_caption("Drone Tests")
 black = (0, 0, 0)
 white = (255,255,255)
 
-drone= Drone()
-drone2 = Drone()
-drone3 = Drone()
-drone4 = Drone()
-drone5 = Drone()
-drone6 = Drone()
-drone7 = Drone()
-drone8 = Drone()
-drone9= Drone()
-drone10 = Drone()
-drone11 = Drone()
-drone12 = Drone()
-drone13 = Drone()
-drone14 = Drone()
-drone15 = Drone()
-drone16 = Drone()
-drone17 = Drone()
-drone18 = Drone()
-drone19 = Drone()
-drone20 = Drone()
-drone21 = Drone()
-drone22 = Drone()
-drone23 = Drone()
-drone24 = Drone()
-drone25 = Drone()
-drone26 = Drone()
-drone27 = Drone()
-drone28 = Drone()
-drone29 = Drone()
-drone30 = Drone()
-drone31 = Drone()
-drone32 = Drone()
-drone33 = Drone()
-drone34 = Drone()
-drone35 = Drone()
+for i in range(35):
+    drone = Drone()
+for i in range(1):
+    dummy = Dummy()
 
 run = True
 while run:
-    pygame.time.delay(25)
-
+    win = pygame.display.set_mode((windowwidth, windowheight))
+    pygame.time.delay(6)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-
 
             run = False
 
     keys = pygame.key.get_pressed()
 
     win.fill(black)
-    drone.tick(keys, win)
-    drone2.tick(keys, win)
-    drone3.tick(keys, win)
-    drone4.tick(keys, win)
-    drone5.tick(keys, win)
-    drone6.tick(keys, win)
-    drone7.tick(keys, win)
-    drone8.tick(keys, win)
-    drone9.tick(keys, win)
-    drone10.tick(keys, win)
-    drone11.tick(keys, win)
-    drone12.tick(keys, win)
-    drone13.tick(keys, win)
-    drone14.tick(keys, win)
-    drone15.tick(keys, win)
-    drone16.tick(keys, win)
-    drone17.tick(keys, win)
-    drone18.tick(keys, win)
-    drone19.tick(keys, win)
-    drone20.tick(keys, win)
-    drone21.tick(keys, win)
-    drone22.tick(keys, win)
-    drone23.tick(keys, win)
-    drone24.tick(keys, win)
-    drone25.tick(keys, win)
-    drone26.tick(keys, win)
-    drone27.tick(keys, win)
-    drone28.tick(keys, win)
-    drone29.tick(keys, win)
-    drone30.tick(keys, win)
-    drone31.tick(keys, win)
-    drone32.tick(keys, win)
-    drone33.tick(keys, win)
-    drone34.tick(keys, win)
-    drone35.tick(keys, win)
+    for d in Drone.droneloc:
+        d.tick(keys, win)
+    for dummy in Dummy.enemyloc:
+        dummy.tick(keys, win)
+
     pygame.display.update()
 
 
