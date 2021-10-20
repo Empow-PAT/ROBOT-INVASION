24f3a2782ef8892d4d56c9b00d9fe7ddd17f90b1
alexroseb <abrenon3@gmail.com>
2021-10-20 09:12:58 -0400

    Javi's changes

--- main.py
+++ main.py
@@ -13,28 +13,51 @@ pygame.init()
 
 windowwidth = 800
 windowheight = 800
-
+Background = pygame.image.load("Art/Robot Invasion Title.jpg")
 win = pygame.display.set_mode((windowwidth, windowheight))
 pygame.display.set_caption("Drone Tests")
 black = (0, 0, 0)
 white = (255,255,255)
 red = (255,0,0)
-for i in range(35):
-    drone = Drone()
-for i in range(20):
-    dummy = Dummy()
+def Play():
+    for i in range(35):
+        drone = Drone()
+    for i in range(20):
+        dummy = Dummy()
 
 
-robot = Normal()
-robot2 = Speedy()
-robot3 = Slow()
+    robot = Normal()
+    robot2 = Speedy()
+    robot3 = Slow()
+    run = True
+    while run:
+
+        pygame.time.delay(25)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        keys = pygame.key.get_pressed()
+
+        win.fill(black)
+        for d in Drone.droneloc:
+            d.tick(keys, win)
+        for dummy in Dummy.enemyloc:
+            dummy.tick(keys, win)
+        for r in robots:
+            r.tick(win)
+
+        pygame.display.update()
+image_surface = pygame.image.load("Art\PlayButton.jpg"),
 manager = pygame_gui.UIManager((800, 800))
 play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                            text='PLAY',
-                                            manager=manager)
+                                        text = "",
+                                        manager=manager)
 clock = pygame.time.Clock()
-run = True
-while run:
+
+run2 = True
+while run2:
     time_delta = clock.tick(60) / 1000.0
     pygame.time.delay(25)
 
@@ -42,43 +65,22 @@ while run:
         if event.type == pygame.QUIT:
 
 
-            run = False
+            run2 = False
 
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == play_button:
                     print('[Insert Text Here]')
+                    Play()
         manager.process_events(event)
     manager.update(time_delta)
 
     keys = pygame.key.get_pressed()
 
     win.fill(black)
+    win.blit(Background,(0,0))
     manager.draw_ui(win)
-    for d in Drone.droneloc:
-        d.tick(keys, win)
-    for dummy in Dummy.enemyloc:
-        dummy.tick(keys, win)
-    for r in robots:
-        r.tick(win)
+
 
     pygame.display.update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
